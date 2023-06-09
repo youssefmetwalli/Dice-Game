b38405341d8ab9f7cd6cc7e36af7566318db7d4e
Youssef Metwalli <ymetwalli@wlan-napt-005.u-aizu.ac.jp>
2023-06-09 16:08:52 +0900

    Won/Lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -1,15 +1,31 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+
         System.out.println("Rolling dice...");
-        
+
         Random random = new Random();
         int dice1 = random.nextInt(6) + 1;
         int dice2 = random.nextInt(6) + 1;
 
         System.out.println("Dice 1: " + dice1);
         System.out.println("Dice 2: " + dice2);
-        System.out.println("Total value: " + (dice1 + dice2));
+        int totalValue = dice1 + dice2;
+        System.out.println("Total value: " + totalValue);
+
+        if (totalValue > 7) {
+            System.out.println(name + " won!");
+        } else {
+            System.out.println(name + " lost!");
+        }
+
+        scanner.close();
     }
 }
