f183ce5e8ec9ef01a7ede6080dc761bea0d2ccac
Youssef Metwalli <ymetwalli@wlan-napt-005.u-aizu.ac.jp>
2023-06-09 16:24:57 +0900

    Greetings Added

--- DiceGame.java
+++ DiceGame.java
@@ -1,15 +1,25 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+
         System.out.println("Rolling dice...");
-        
+
         Random random = new Random();
         int dice1 = random.nextInt(6) + 1;
         int dice2 = random.nextInt(6) + 1;
 
-        System.out.println("Die 1: " + dice1);
-        System.out.println("Die 2: " + dice2);
-        System.out.println("Total value: " + (dice1 + dice2));
+        System.out.println("Dice 1: " + dice1);
+        System.out.println("Dice 2: " + dice2);
+        int totalValue = dice1 + dice2;
+        System.out.println("Total value: " + totalValue);
+
+        scanner.close();
     }
 }
